7bc567275a931541d69f27087d0b548ba9491ef9
bengyman <230474P@mymail.nyp.edu.sg>
2024-07-08 14:13:35 +0800

    adadad

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './components.css';
-import AnimatedTrail from './AnimatedTrail'; // Replace with the correct import path
+import AnimatedTrail from './AnimatedTrail';
+import floralHavenImage from '../images/WhatsApp Image 2024-07-08 at 13.55.24_9e9e16cb.jpg'; // Adjust the path as needed
 
 const Projects = () => {
   return (
@@ -11,10 +12,14 @@ const Projects = () => {
         Ecoutopia is a collaborative project where my team and I developed a fully functional e-commerce website specializing in plant sales. Leveraging HTML, Flask, JavaScript, CSS, and Bootstrap, we created a seamless user experience with robust backend functionality. This project allowed me to enhance my skills in web development, teamwork, and problem-solving, contributing to a successful implementation that met both functional and aesthetic goals.
       </p>
 
+      <img src={floralHavenImage} alt="FloralHaven Project" className="project-image" />
+
       <p className="project-description">
         FloralHaven<br />
         FloralHaven is a website for community center residents. It offers courses that teach about sustainability and has other functionalities like rewards and a forum. It is currently a work in progress.
       </p>
+
+      
 
       <p>
         I also have multiple smaller projects that I am currently working on. Here is one using React!:
